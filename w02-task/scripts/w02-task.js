9690ac3d5d3a336a753f52fc325164b2e333495c
Ahmed Fofana <82991017+ahmfof@users.noreply.github.com>
2023-11-18 22:10:05 -0700

    done

--- w02-task/scripts/w02-task.js
+++ w02-task/scripts/w02-task.js
@@ -1,35 +1,57 @@
-/* W02-Task - Profile Home Page */
+// /* W02-Task - Profile Home Page */
 
-/* Step 1 - Setup type tasks - no code required */
+// /* Step 1 - Setup type tasks - no code required */
 
-/* Step 2 - Variables */
+// /* Step 2 - Variables */
 
-let fullName = 'Ahmed Fofana';
-let currentYear = new Date().getFullYear(); // Use this to get the current year
-let profilePicture = 'images/me.jpeg';
+// const fullName = 'Ahmed Fofana';
+// let currentYear = new Date().getFullYear(); // Use this to get the current year
+// let profilePicture = 'images/me.jpeg';
 
-/* Step 3 - Element Variables */
+// /* Step 3 - Element Variables */
+// const nameElement = document.getElementById('Ahmed Fofana'); // Define nameElement
+// const yearElement = document.getElementById('years').innerHTML =currentYear; // Define yearElement
+// const foodElement = document.getElementById('food');
 
-const nameElement = document.getElementById('Ahmed Fofana'); // Define nameElement
-const yearElement = document.getElementById('2023'); // Define yearElement
-const foodElement = document.getElementById('food');
+// /* Step 4 - Adding Content */
 
-/* Step 4 - Adding Content */
+// nameElement.innerHTML = `<strong>${fullName}</strong>`;
+// yearElement.textContent = currentYear;
+// (document.getElementById('image')).setAttribute('src', profilePicture);
 
-nameElement.innerHTML = `<strong>${fullName}</strong>`;
-yearElement.textContent = currentYear;
-(document.getElementById('image')).setAttribute('src', profilePicture);
+// /* Step 5 - Array */
+// let favFood1;
+// let favfood2;
+// let favoriteFood;
+// favFood1 = [" Tacos", " Sushi", " Hamburgers"];
+// document.getElementById("food").innerHTML = favFood1;
+// favfood2 = " Teriyaki Chicken";
+// favoriteFood = favFood1.concat(favfood2);
+// document.getElementById("food").innerHTML = favoriteFood;
+// favoriteFood.splice(0,1);
+// document.getElementById("food").innerHTML = favoriteFood;
+// favoriteFood.splice(2,1);
+// document.getElementById("food").innerHTML = favoriteFood;
 
-/* Step 5 - Array */
-let favFood1;
-let favfood2;
-let favoriteFood;
-favFood1 = [" Tacos", " Sushi", " Hamburgers"];
-document.getElementById("food").innerHTML = favFood1;
-favfood2 = " Teriyaki Chicken";
-favoriteFood = favFood1.concat(favfood2);
-document.getElementById("food").innerHTML = favoriteFood;
-favoriteFood.splice(0,1);
-document.getElementById("food").innerHTML = favoriteFood;
-favoriteFood.splice(2,1);
-document.getElementById("food").innerHTML = favoriteFood;
+
+const fullName = 'Ahmed Fofana';
+document.querySelector('#name').textContent = fullName;
+
+const currentYear = 2023;
+document.querySelector('#years').textContent = currentYear;
+
+const profilePicture ='images/me.jpeg';
+document.querySelector('img').setAttribute('src', profilePicture);
+
+const favoriteFood = ['Placali','Sauce Graine','Fish Soup'];
+document.querySelector('#food').textContent = favoriteFood;
+
+const anotherFavoriteFood = ['Hamburgers', 'McdDonal'];
+favoriteFood.push(anotherFavoriteFood);
+document.querySelector('#food').textContent = favoriteFood;
+
+favoriteFood.shift();
+document.querySelector('#food').textContent = favoriteFood;
+
+favoriteFood.pop();
+document.querySelector('#food').textContent = favoriteFood;
---
+++ w03-task/w03-task.js
@@ -0,0 +1,86 @@
+/* LESSON 3 - Programming Tasks */
+
+/* FUNCTIONS */
+/* Function Definition - Add Numbers */
+
+
+function add(number1, number2) {
+    return number1 + number2;
+}
+function addNumbers() {
+    let addend1 = parseInt(document.querySelector('#addend1').value);
+    let addend2 = parseInt(document.querySelector('#addend2').value);
+    let result = add(addend1, addend2);
+    document.querySelector('#sum').value = result;
+}
+document.querySelector('#addNumbers').addEventListener('click', addNumbers);
+
+/* Function Expression - Subtract Numbers */
+const subtract = function (number1, number2) {
+    return number1 - number2;
+}
+const subtractNumbers = function() {
+    let minuend = parseInt(document.querySelector('#minuend').value);
+    let subtrahend = parseInt(document.querySelector('#subtrahend').value);
+    let result = subtract(minuend, subtrahend);
+    document.querySelector('#difference').value = result;
+}
+document.querySelector('#subtractNumbers').addEventListener('click', subtractNumbers);
+
+/* Arrow Function - Multiply Numbers */
+const multiply = (number1, number2) => number1 * number2;
+const multiplyNumbers = () => {
+    let factor1 = parseInt(document.querySelector('#factor1').value);
+    let factor2 = parseInt(document.querySelector('#factor2').value);
+    let result = multiply(factor1, factor2);
+    document.querySelector('#product').value = result;
+}
+document.querySelector('#multiplyNumbers').addEventListener('click', multiplyNumbers);
+
+/* Open Function Use - Divide Numbers */
+function divide(number1, number2) {
+    return number1 / number2;
+}
+function divideNumbers() {
+    let dividend = parseInt(document.querySelector('#dividend').value);
+    let divisor = parseInt(document.querySelector('#divisor').value);
+    let result = divide(dividend, divisor);
+    document.querySelector('#quotient').value = result;
+}
+document.querySelector('#divideNumbers').addEventListener('click', divideNumbers);
+
+
+/* Decision Structure */
+const currentDate = new Date();
+let currentYear;
+currentYear = currentDate.getFullYear();
+document.querySelector('#year').textContent = currentYear;
+
+/* ARRAY METHODS - Functional Programming */
+/* Output Source Array */
+const numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13];
+document.querySelector('#array').textContent = numbers;
+
+/* Output Odds Only Array */
+document.querySelector('#odds').textContent = numbers.filter(number => number % 2);
+
+/* Output Evens Only Array */
+document.querySelector('#Evens').textContent = numbers.filter(number => number % 2===0);
+
+/* Output Sum of Org. Array */
+document.querySelector('#sumOfArray').textContent = numbers.reduce((sum, number) => sum + number, 0);
+
+/* Output Multiplied by 2 Array */
+document.querySelector('#multiplied').textContent = numbers.map(number => number * 2);
+
+/* Output Sum of Multiplied by 2 Array */
+fofana = sumOfMultiplied;
+document.querySelector('#sumOfMultiplied').textContent = numbers.map(number => number * 2).reduce((sum, number) => sum + number, 0);
+
+
+
+
+
+
+
+  
